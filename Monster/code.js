d0e8612ea7343d413dd35e6c535eafe7298ad583
Murilo <161628769+Murilo1006@users.noreply.github.com>
2025-02-26 14:52:24 -0300

    Add files via upload

---
+++ Monster/code.js
@@ -0,0 +1,9 @@
+function banco(){
+    const bd = [
+                {fruta1:'Laranja', cor: 'amarelo', tamanho: 'pequena'}, //0
+                {fruta:'Melão', cor: 'verde'}//1
+            ]
+
+    console.log(bd[0])
+}
+banco()
